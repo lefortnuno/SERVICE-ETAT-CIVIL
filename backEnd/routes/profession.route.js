92398b3cffe71c6afc6b6aeb0a8871f292c4f260
lefortnuno <trofelnuno@gmail.com>
2024-12-02 17:11:08 +0100

    e-CIN Online DoD

--- backEnd/routes/profession.route.js
+++ backEnd/routes/profession.route.js
@@ -2,6 +2,7 @@ const router = require("express").Router();
 const ProfessionController = require("../controllers/profession.controller");
 
 router.get("/", ProfessionController.getAllProfessions);
+router.get("/glitch/", ProfessionController.getAllProfessions);
 router.get("/:idProfession", ProfessionController.getidProfession);
 router.post("/", ProfessionController.addProfession);
 router.put("/:idProfession", ProfessionController.updateProfession);
@@ -9,4 +10,3 @@ router.get("/recherche/:valeur", ProfessionController.searchdProfession);
 router.delete("/:idProfession", ProfessionController.deleteProfession);
 
 module.exports = router;
-
--- frontEnd/src/components/login/SeConnecter.js
+++ frontEnd/src/components/login/SeConnecter.js
@@ -6,57 +6,75 @@ import HeaderContext from "../design/header";
 import LogoContext from "../design/logo";
 import SideBarContext from "../design/sidebar";
 import FooterContext from "../design/footer";
-
-import { BsArrowRight } from "react-icons/bs";
+import axios from "../../api/axios";
+import { useEffect } from "react";
 
 export default function SeConnecter() {
-	return (
-		<>
-			{libraryList.forEach((x) => AjoutLibrary(x))}
+  useEffect(() => {
+    const fetchData = () => {
+      axios
+        .get("profession/glitch/")
+        .then(function (response) {
+          //   console.log("Données récupérées : ", response.data);
+        })
+        .catch((error) => {
+          //   console.error("Erreur lors de la récupération : ", error);
+        });
+    };
 
-			<div className="wrapper">
-				<div className="main-header">
-					<LogoContext />
-					<div className="logo-header">
-						<p className="logo">
-							<img
-								src={process.env.PUBLIC_URL + `/picture/logo/e-CIN.png`}
-								alt="pdp"
-								style={{ width: "80%", height: "100%", borderRadius: "0%" }}
-							/>
-						</p>
-					</div>
+    fetchData();
+    const interval = setInterval(fetchData, 60000); // 1.MIN
+    return () => clearInterval(interval);
+  }, []);
+  return (
+    <>
+      {libraryList.forEach((x) => AjoutLibrary(x))}
 
-					<nav className="navbar navbar-header navbar-expand-lg">
-						<div className="container-fluid">
-							<HeaderContext />
-						</div>
-					</nav>
-				</div>
+      <div className="wrapper">
+        <div className="main-header">
+          <LogoContext />
+          <div className="logo-header">
+            <p className="logo">
+              <img
+                src={process.env.PUBLIC_URL + `/picture/logo/e-CIN.png`}
+                alt="pdp"
+                style={{ width: "80%", height: "100%", borderRadius: "0%" }}
+              />
+            </p>
+          </div>
 
-				<SideBarContext />
+          <nav className="navbar navbar-header navbar-expand-lg">
+            <div className="container-fluid">
+              <HeaderContext />
+            </div>
+          </nav>
+        </div>
 
-				<div className="main-panel">
-					<div className="content">
-						<div className="container-fluid">
-							<div className="container-login100">
-								<div className="wrap-login100">
-									<div className="login100-pic js-tilt" data-tilt>
-										<img
-											src={process.env.PUBLIC_URL + `/picture/logo/fanjakana.jpg`}
-											alt="image"
-										/>
-									</div>
+        <SideBarContext />
 
-									<FormulaireSeConnecter />
-								</div>
-							</div>
-						</div>
-					</div>
+        <div className="main-panel">
+          <div className="content">
+            <div className="container-fluid">
+              <div className="container-login100">
+                <div className="wrap-login100">
+                  <div className="login100-pic js-tilt" data-tilt>
+                    <img
+                      src={
+                        process.env.PUBLIC_URL + `/picture/logo/fanjakana.jpg`
+                      }
+                      alt="image"
+                    />
+                  </div>
 
-					<FooterContext />
-				</div>
-			</div>
-		</>
-	);
+                  <FormulaireSeConnecter />
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <FooterContext />
+        </div>
+      </div>
+    </>
+  );
 }
